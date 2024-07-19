a193f66d28dc81abd21bdc09c4f5b2c3e1914656
wibus-wee <1596355173@qq.com>
2024-07-19 17:55:31 +0800

    Initial Commit

---
+++ InjectGUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  InjectGUI
+//
+//  Created by wibus on 2024/7/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InjectGUI/InjectGUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InjectGUIApp.swift
+//  InjectGUI
+//
+//  Created by wibus on 2024/7/19.
+//
+
+import SwiftUI
+
+@main
+struct InjectGUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
