7a8cfc5d21e0f630ea22e7ff8551f5dd41c0f088
wibus-wee <1596355173@qq.com>
2024-09-09 23:44:21 +0800

    chore: remove cache module

--- InjectGUI/Backend/Cache.swift
+++ InjectGUI/Backend/Cache.swift
@@ -4,17 +4,17 @@
 //
 //  Created by wibus on 2024/7/31.
 //
-
-import Cache
-
-enum CacheKey {
-    static let latestCommitHash = "latestCommitHash"
-}
-
-// MARK: - Cache Configuration
-
-// 存一个有关 main/latest commit hash 的缓存
-let diskConfig = DiskConfig(name: "InjectLib")
-let memoryConfig = MemoryConfig(expiry: .never, countLimit: 10, totalCostLimit: 10)
-let transformer = TransformerFactory.forCodable(ofType: String.self)
-let storage = try! Storage<String, String>(diskConfig: diskConfig, memoryConfig: memoryConfig, transformer: transformer)
+//
+//import Cache
+//
+//enum CacheKey {
+//    static let latestCommitHash = "latestCommitHash"
+//}
+//
+//// MARK: - Cache Configuration
+//
+//// 存一个有关 main/latest commit hash 的缓存
+//let diskConfig = DiskConfig(name: "InjectLib")
+//let memoryConfig = MemoryConfig(expiry: .never, countLimit: 10, totalCostLimit: 10)
+//let transformer = TransformerFactory.forCodable(ofType: String.self)
+//let storage = try! Storage<String, String>(diskConfig: diskConfig, memoryConfig: memoryConfig, transformer: transformer)
